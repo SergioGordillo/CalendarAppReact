e55156369a4553a618b758c24a8178182ffc3767
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-12-18 23:34:15 +0100

    feat: React Big Calendar installed and PoC of CalendarScreen done

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import { Navbar } from '../ui/Navbar'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+    title: "Carmela's Birthday",
+    start: moment().toDate(),
+    end: moment().add(2,'hours').toDate(),
+    bgcolor: '#fafafa'
+}]
 
 export const CalendarScreen = () => {
     return (
-        <div>
-            <Navbar/>
+        <div className='calendar-screen'>
+            <Navbar />
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+            />
         </div>
     )
 }
