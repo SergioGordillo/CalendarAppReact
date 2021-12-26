e534bcdd11ff96cb1ea8c87c083eece63a7bfee5
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-12-26 21:10:49 +0100

    feat: configuration of Redux done

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import { uiReducer } from './uiReducer';
+
+
+
+export const rootReducer = combineReducers({
+    ui: uiReducer,
+    // TODO: AuthReducer
+    // TODO: CalendarReducer
+})
